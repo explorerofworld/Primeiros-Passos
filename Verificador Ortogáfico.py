05773231b61c7812f2bba25e0c7f68c11e2fcb6b
Sergio Ferreira <98838215+explorerofworld@users.noreply.github.com>
2022-02-26 11:53:51 -0300

    Verificador Ortográfico
    Utilizado para verificar a necessidade de crase.

---
+++ Verificador Ortogáfico.py
@@ -0,0 +1,13 @@
+print('Verificador de crase na frase:vou a ...')
+
+x=input('Digite o estado:')
+
+b=(x[-1])
+c =('a')
+
+if (b==c):
+    print('Vou à'+' '+ x)
+
+else:
+    print('Vou a'+' '+ x)
+
